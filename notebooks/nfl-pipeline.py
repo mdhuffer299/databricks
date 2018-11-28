e41f507b92a9e744fc56b515d5ee466c553e009f
Michael Huffer <33228313+mdhuffer299@users.noreply.github.com>
2018-11-27 22:04:20 -0500

    Update: README.md nfl-playtype-pred.ipynb notebooks

---
+++ notebooks/nfl-pipeline.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %run /Users/mhuffer@allegient.com/nfl-data-ingestion
+
+# COMMAND ----------
+
+# MAGIC %run /Users/mhuffer@allegient.com/nfl-data-cleanup
+
+# COMMAND ----------
+
+# MAGIC %run /Users/mhuffer@allegient.com/nfl-train-play-type
+
+# COMMAND ----------
+
+#%run /Users/mhuffer@allegient.com/nfl-file-removal
+
+# COMMAND ----------
+
