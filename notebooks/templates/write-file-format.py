632c7425330221e4198bc949fc660b2ad008ce3c
Michael Huffer <33228313+mdhuffer299@users.noreply.github.com>
2019-02-21 20:30:47 -0500

    Update: README.md academy nfl-playtype-pred.ipynb notebooks

---
+++ notebooks/templates/write-file-format.py
@@ -0,0 +1,52 @@
+# Databricks notebook source
+dbutils.widgets.text("dataframe", "No Dataframe Defined")
+dbutils.widgets.text("outputPath", "No Path Defined")
+dbutils.widgets.dropdown("outputFileType", "csv", ["csv", "json", "parquet", "orc"])
+
+# COMMAND ----------
+
+#dbutils.widgets.removeAll()
+
+# COMMAND ----------
+
+inputDF = dbutils.widgets.get("dataframe")
+outputPath = dbutils.widgets.get("outputPath")
+outputFileType = dbutils.widgets.get("outputFileType")
+
+# COMMAND ----------
+
+global_temp_db = spark.conf.get("spark.sql.globalTempDatabase")
+df = table(global_temp_db + "." + inputDF)
+
+# COMMAND ----------
+
+try:
+  dirExists = dbutils.fs.ls(outputPath)
+except:
+  dirExists = False
+
+if dirExists == False:
+  dbutils.fs.mkdirs(outputPath)
+  print(outputPath + " has been created")
+else:
+  print("Output directory already exists!")
+
+# COMMAND ----------
+
+(df
+ .write
+ .format(outputFileType)
+ .mode("overwrite")
+ .save(outputPath)
+)
+
+# COMMAND ----------
+
+dbutils.notebook.exit("Notebook Completed: Data was loaded to " + outputPath)
+
+# COMMAND ----------
+
+#dbutils.fs.rm("/tmp/" + outputFileType, True)
+
+# COMMAND ----------
+
